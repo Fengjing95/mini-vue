646908711ab839c11258d1f34c0a90e5696d9ee4
liuhao <1984779164@qq.com>
2022-07-03 15:14:55 +0800

    feat: scheduler

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -3,24 +3,39 @@
  * @Author: 枫
  * @LastEditors: 枫
  * @description: description
- * @LastEditTime: 2022-06-29 21:21:09
+ * @LastEditTime: 2022-07-03 15:14:30
  */
 class ReactiveEffect {
-  _fn: Function
-  constructor(fn: Function) {
+  private _fn: Function
+  public scheduler?: Function
+
+  constructor(fn: Function, scheduler?: Function) {
     this._fn = fn
+    this.scheduler = scheduler
   }
 
   run() {
     activeEffect = this
-    this._fn()
+    return this._fn()
   }
+}
+
+let activeEffect: ReactiveEffect;
+
+export function effect(fn: Function, options: any = {}) {
+  const _effect = new ReactiveEffect(fn, options.scheduler)
+
+  _effect.run()
+  
+
+  const runner = _effect.run.bind(_effect)
+  return runner
 }
 
 
 const targetMap = new WeakMap<object, Map<keyof any, Set<ReactiveEffect>>>()
 
-export function track<T extends object>(target: T, key: keyof T) { 
+export function track<T extends object>(target: T, key: keyof T) {
   // target -> key -> dep
 
   let depsMap = targetMap.get(target)
@@ -38,21 +53,15 @@ export function track<T extends object>(target: T, key: keyof T) {
 
 }
 
-
-
-let activeEffect: ReactiveEffect;
-export function effect(fn: Function) {
-  const _effect = new ReactiveEffect(fn)
-  
-  _effect.run()
-}
-
 export function trigger<T extends object>(target: T, key: keyof T) {
   let depsMap = targetMap.get(target)
 
   let dep = depsMap!.get(key) as Set<ReactiveEffect>
 
   for (const effect of dep) {
-    effect.run()
+    if (effect.scheduler)
+      effect.scheduler()
+    else
+      effect.run()
   }
 }
